bd0184e05e1959d876964a71eef94506dd841dd1
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2025-05-03 11:15:29 +0530

    connected to database

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
 
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 8000;
+const mongoUri = process.env.MONGO_URI;
 
 app.get("/", (req, res) => {
   res.send(`Hello world`);
 });
 
-app.listen(port, () => {
-  console.log(`server is running on http://localhost:${port}`);
+mongoose.connect(mongoUri).then(() => {
+  console.log(`server is connected to database`);
+  app.listen(port, () => {
+    console.log(`server is running on http://localhost:${port}`);
+  });
 });
