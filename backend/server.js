51cdf9d5ece2ec592170061cb0bdc34f16f2cac7
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2025-05-06 16:36:11 +0530

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
-import { fileURLToPath } from "url"; // Import this to get the current file's path
-
 import connectDB from "./config/db.js";
 import ProductRouter from "./routes/ProductRouter.js";
 import cartRouter from "./routes/cartRouter.js";
@@ -12,34 +10,29 @@ dotenv.config();
 const app = express();
 connectDB();
 
-// Get the current directory of the module (to replace __dirname in ES modules)
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Middleware
+//middlwares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Serve static files from the frontend build directory
-if (process.env.NODE_ENV === "production") {
-  // Serve the frontend build files
-  app.use(express.static(path.join(__dirname, "frontend", "dist")));
-
-  // Serve index.html for all routes (for React Router)
-  app.get("/{*splat}", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
-
 const port = process.env.PORT || 8000;
-
-app.get("/", (req, res) => {
-  res.send(`Hello world`);
-});
 
 app.use("/api/products", ProductRouter);
 app.use("/api/cart", cartRouter);
 
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`server is running on http://localhost:${port}`);
 });
